b3adf50ce30cebe38f0259489baa52c9f6da8c44
Josh <jjmiklos1@gmail.com>
2024-09-12 13:17:01 -0600

    Completed assignment.

--- average.py
+++ average.py
@@ -1,12 +1,21 @@
-"""
-Import the text_utils module you created and calculate the average number of
-words per line in a given text file. The text file that will be used to test
-this is the text file called "sample.txt" (located in the same directory as
-this exercise). The average number of words per line should be rounded down to
-the nearest integer.
+import text_utils                                                   # Imports the code that counts the number of characters, words, and sentences.
 
-Print the average number of words per line in the text file in the following
-format:
+def line_count():
+    
+    file = open('sample.txt', 'r')                                  # Opens the file in read-only mode.
 
-"Average words per line: [average]"
-"""
+    lines = len(file.readlines())                                   # Counts the number of lines in the file.
+
+    file.seek(0)                                                    # Goes back to the start of the file, so it can be reread.
+
+    word_total = text_utils.count_words(file.read())                # Rereads the file to count how many words there are.
+    
+    file.close()                                                    # Closes the file.
+
+    average = word_total // lines                                   # Divided the number of words by the number of lines, and rounds down.
+
+    return (f"Average words per line: {average}")                   # Returns the average words per line.
+
+line_count()
+lc = line_count()
+print(lc)                                                           # Prints the line_count function.
